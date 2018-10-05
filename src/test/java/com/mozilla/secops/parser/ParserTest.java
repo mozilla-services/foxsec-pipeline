5ccb2c21f64040fe71aba57fbbf937939847378f
Aaron Meihm <ameihm@mozilla.com>
2018-10-05 17:40:41 -0500

    add test for raw openssh parser input

--- src/test/java/com/mozilla/secops/parser/ParserTest.java
+++ src/test/java/com/mozilla/secops/parser/ParserTest.java
@@ -94,7 +94,28 @@ public class ParserTest {
     }
 
     @Test
-    public void testOpenSSH() throws Exception {
+    public void testOpenSSHRaw() throws Exception {
+        String buf = "Sep 18 22:15:38 emit-bastion sshd[2644]: Accepted publickey for riker from 12" +
+            "7.0.0.1 port 58530 ssh2: RSA SHA256:dd/AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+        Parser p = new Parser();
+        assertNotNull(p);
+        Event e = p.parse(buf);
+        assertNotNull(e);
+        assertEquals(Payload.PayloadType.OPENSSH, e.getPayloadType());
+        OpenSSH o = e.getPayload();
+        assertNotNull(o);
+        assertEquals("riker", o.getUser());
+        assertEquals("publickey", o.getAuthMethod());
+        assertEquals("127.0.0.1", o.getSourceAddress());
+        Normalized n = e.getNormalized();
+        assertNotNull(n);
+        assertEquals(Normalized.Type.AUTH, n.getType());
+        assertEquals("riker", n.getSubjectUser());
+        assertEquals("127.0.0.1", n.getSourceAddress());
+    }
+
+    @Test
+    public void testOpenSSHStackdriver() throws Exception {
         String buf = "{\"insertId\":\"f8p4mz1a3ldcos1xz\",\"labels\":{\"compute.googleapis.com/resource_" +
             "name\":\"emit-bastion\"},\"logName\":\"projects/sandbox-00/logs/syslog\",\"receiveTimestamp\"" +
             ":\"2018-09-20T18:43:38.318580313Z\",\"resource\":{\"labels\":{\"instance_id\":\"9999999999999" +
