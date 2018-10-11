36ef68164abeffc417405ddfa13e0f48c80dfdc5
Aaron Meihm <ameihm@mozilla.com>
2018-10-11 16:13:21 -0500

    add test for HTTPRequest.CountInWindow

--- src/test/java/com/mozilla/secops/httprequest/TestThresholdAnalysis1.java
+++ src/test/java/com/mozilla/secops/httprequest/TestThresholdAnalysis1.java
@@ -11,6 +11,7 @@ import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.transforms.windowing.IntervalWindow;
 import org.apache.beam.sdk.transforms.Combine;
 import org.apache.beam.sdk.transforms.Count;
+import org.apache.beam.sdk.values.KV;
 
 import org.joda.time.Instant;
 
@@ -19,6 +20,7 @@ import com.mozilla.secops.parser.Event;
 import java.io.IOException;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.zip.GZIPInputStream;
 
 public class TestThresholdAnalysis1 {
@@ -58,6 +60,34 @@ public class TestThresholdAnalysis1 {
         PAssert.that(count)
             .inWindow(new IntervalWindow(new Instant(300000L), new Instant(360000)))
             .containsInAnyOrder(2520L);
+
+        p.run().waitUntilFinish();
+    }
+
+    @Test
+    public void countInWindowTest() throws Exception {
+        ArrayList<KV<String, Long>> expect = new ArrayList<KV<String, Long>>(Arrays.asList(
+                KV.of("192.168.1.1", 60L),
+                KV.of("192.168.1.2", 60L),
+                KV.of("192.168.1.3", 60L),
+                KV.of("192.168.1.4", 60L),
+                KV.of("192.168.1.5", 60L),
+                KV.of("192.168.1.6", 60L),
+                KV.of("192.168.1.7", 60L),
+                KV.of("192.168.1.8", 60L),
+                KV.of("192.168.1.9", 60L),
+                KV.of("192.168.1.10", 60L),
+                KV.of("10.0.0.1", 900L),
+                KV.of("10.0.0.2", 900L)
+            ));
+        PCollection<String> input = getInput();
+
+        PCollection<KV<String, Long>> counts = input.apply(new HTTPRequest.ParseAndWindow())
+            .apply(new HTTPRequest.CountInWindow());
+
+        PAssert.that(counts)
+            .inWindow(new IntervalWindow(new Instant(0L), new Instant(60000)))
+            .containsInAnyOrder(expect);
 
         p.run().waitUntilFinish();
     }
