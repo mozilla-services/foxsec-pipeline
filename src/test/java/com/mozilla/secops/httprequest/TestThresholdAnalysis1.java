d0210a40db1f77bbda78b5596a98f00f755d5aa2
Aaron Meihm <ameihm@mozilla.com>
2018-10-11 16:54:12 -0500

    add test for HTTPRequest.ThresholdAnalysis

--- src/test/java/com/mozilla/secops/httprequest/TestThresholdAnalysis1.java
+++ src/test/java/com/mozilla/secops/httprequest/TestThresholdAnalysis1.java
@@ -3,6 +3,9 @@ package com.mozilla.secops.httprequest;
 import org.junit.Test;
 import org.junit.Rule;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.anyOf;
+import static org.hamcrest.Matchers.equalTo;
 
 import org.apache.beam.sdk.testing.TestPipeline;
 import org.apache.beam.sdk.testing.PAssert;
@@ -88,6 +91,38 @@ public class TestThresholdAnalysis1 {
         PAssert.that(counts)
             .inWindow(new IntervalWindow(new Instant(0L), new Instant(60000)))
             .containsInAnyOrder(expect);
+
+        p.run().waitUntilFinish();
+    }
+
+    @Test
+    public void thresholdAnalysisTest() throws Exception {
+        PCollection<String> input = getInput();
+
+        PCollection<Result> results = input.apply(new HTTPRequest.ParseAndWindow())
+            .apply(new HTTPRequest.CountInWindow())
+            .apply(new HTTPRequest.ThresholdAnalysis(1.0));
+
+        PCollection<Long> resultCount = results.apply(Combine.globally(Count.<Result>combineFn())
+                .withoutDefaults());
+        PAssert.that(resultCount)
+            .inWindow(new IntervalWindow(new Instant(300000L), new Instant(360000L)))
+            .containsInAnyOrder(2L);
+
+        PAssert.that(results)
+            .inWindow(new IntervalWindow(new Instant(300000L), new Instant(360000L)))
+            .satisfies(
+                i -> {
+                    for (Result r : i) {
+                        assertThat(r.getSourceAddress(), anyOf(equalTo("10.0.0.1"), equalTo("10.0.0.2")));
+                        assertEquals(900L, (long)r.getCount());
+                        assertEquals(180.0, (double)r.getMeanValue(), 0.1);
+                        assertEquals(1.0, (double)r.getThresholdModifier(), 0.1);
+                        assertEquals(359999L, r.getWindowTimestamp().getMillis());
+                    }
+                    return null;
+                }
+            );
 
         p.run().waitUntilFinish();
     }
