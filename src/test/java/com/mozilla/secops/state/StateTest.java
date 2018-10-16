180995366036dc40fe029d58cb42c3c327941cbc
Aaron Meihm <ameihm@mozilla.com>
2018-10-15 23:02:42 -0500

    remove SimpleStateInterface

--- src/test/java/com/mozilla/secops/state/StateTest.java
+++ src/test/java/com/mozilla/secops/state/StateTest.java
@@ -29,7 +29,6 @@ public class StateTest {
     @Parameters
     public static Collection<Object[]> data() {
         return Arrays.asList(new Object[][] {
-            { new SimpleStateInterface() },
             { new MemcachedStateInterface("127.0.0.1", 11211) },
             { new DatastoreStateInterface("test") }
         });
