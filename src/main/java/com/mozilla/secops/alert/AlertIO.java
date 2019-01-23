5f8934bfcfe8dbe77412d9be23f3dbcf16cd6522
Aaron Meihm <ameihm@mozilla.com>
2019-01-23 16:48:35 -0600

    merge alerts based on key metadata in AlertIO

--- src/main/java/com/mozilla/secops/alert/AlertIO.java
+++ src/main/java/com/mozilla/secops/alert/AlertIO.java
@@ -1,11 +1,20 @@
 package com.mozilla.secops.alert;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.UUID;
 import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.GroupByKey;
 import org.apache.beam.sdk.transforms.PTransform;
 import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.transforms.windowing.AfterProcessingTime;
+import org.apache.beam.sdk.transforms.windowing.GlobalWindows;
+import org.apache.beam.sdk.transforms.windowing.Repeatedly;
+import org.apache.beam.sdk.transforms.windowing.Window;
+import org.apache.beam.sdk.values.KV;
 import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.values.PDone;
+import org.joda.time.Duration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -48,12 +57,60 @@ public class AlertIO {
 
     @Override
     public PDone expand(PCollection<String> input) {
-      input.apply(ParDo.of(new WriteFn(this)));
+      input
+          .apply(
+              Window.<String>into(new GlobalWindows())
+                  .triggering(
+                      Repeatedly.forever(
+                          AfterProcessingTime.pastFirstElementInPane()
+                              .plusDelayOf(Duration.standardMinutes(1)))))
+          .apply(
+              ParDo.of(
+                  new DoFn<String, KV<String, Alert>>() {
+                    private static final long serialVersionUID = 1L;
+
+                    @ProcessElement
+                    public void processElement(ProcessContext c) {
+                      Alert a = Alert.fromJSON(c.element());
+                      if (a == null) {
+                        return;
+                      }
+                      String key = a.getNotifyMergeKey();
+                      if (key == null) {
+                        key = UUID.randomUUID().toString();
+                      }
+                      c.output(KV.of(key, a));
+                    }
+                  }))
+          .apply(GroupByKey.<String, Alert>create())
+          .apply(
+              ParDo.of(
+                  new DoFn<KV<String, Iterable<Alert>>, Alert>() {
+                    private static final long serialVersionUID = 1L;
+
+                    @ProcessElement
+                    public void processElement(ProcessContext c) {
+                      Iterable<Alert> alerts = c.element().getValue();
+                      if (alerts == null) {
+                        return;
+                      }
+                      Alert[] a = ((Collection<Alert>) alerts).toArray(new Alert[0]);
+                      if (a.length < 1) {
+                        return;
+                      } else if (a.length == 1) {
+                        c.output(a[0]);
+                        return;
+                      }
+                      a[0].addMetadata("notify_merged_count", Integer.toString(a.length));
+                      c.output(a[0]);
+                    }
+                  }))
+          .apply(ParDo.of(new WriteFn(this)));
       return PDone.in(input.getPipeline());
     }
   }
 
-  private static class WriteFn extends DoFn<String, Void> {
+  private static class WriteFn extends DoFn<Alert, Void> {
     private static final long serialVersionUID = 1L;
 
     private final Write wTransform;
@@ -85,11 +142,9 @@ public class AlertIO {
 
     @ProcessElement
     public void processElement(ProcessContext c) {
-      String raw = c.element();
-      Alert a = Alert.fromJSON(raw);
-      if (a == null) {
-        return;
-      }
+      Alert a = c.element();
+      String raw = a.toJSON();
+
       log.info("processing alert: {}", raw);
 
       if (!a.hasCorrectFields()) {
