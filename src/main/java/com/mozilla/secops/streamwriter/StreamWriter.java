4d5e51eb4fae567f9a89eb54a814df7fc05b1d8d
Aaron Meihm <ameihm@mozilla.com>
2019-03-05 10:43:37 -0600

    add simple IO stream writer

---
+++ src/main/java/com/mozilla/secops/streamwriter/StreamWriter.java
@@ -0,0 +1,42 @@
+package com.mozilla.secops.streamwriter;
+
+import com.mozilla.secops.CompositeInput;
+import com.mozilla.secops.InputOptions;
+import com.mozilla.secops.OutputOptions;
+import java.io.IOException;
+import java.io.Serializable;
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+
+/**
+ * Simple IO stream writer
+ *
+ * <p>Connects composite input with output transforms.
+ */
+public class StreamWriter implements Serializable {
+  private static final long serialVersionUID = 1L;
+
+  /** Runtime options for {@link StreamWriter} pipeline. */
+  public interface StreamWriterOptions extends PipelineOptions, InputOptions, OutputOptions {}
+
+  private static void runStreamWriter(StreamWriterOptions options) throws IOException {
+    Pipeline p = Pipeline.create(options);
+
+    p.apply("input", new CompositeInput(options)).apply(OutputOptions.compositeOutput(options));
+
+    p.run();
+  }
+
+  /**
+   * Entry point for Beam pipeline.
+   *
+   * @param args Runtime arguments.
+   */
+  public static void main(String[] args) throws IOException {
+    PipelineOptionsFactory.register(StreamWriterOptions.class);
+    StreamWriterOptions options =
+        PipelineOptionsFactory.fromArgs(args).withValidation().as(StreamWriterOptions.class);
+    runStreamWriter(options);
+  }
+}
