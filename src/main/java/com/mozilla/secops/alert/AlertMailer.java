2774b3050c5064c9563fcbd523b841049254791c
Aaron Meihm <ameihm@mozilla.com>
2019-04-04 12:05:02 -0500

    raise existing IOException in AlertMailer init

--- src/main/java/com/mozilla/secops/alert/AlertMailer.java
+++ src/main/java/com/mozilla/secops/alert/AlertMailer.java
@@ -102,16 +102,11 @@ public class AlertMailer {
    *
    * @param cfg {@link AlertConfiguration}
    */
-  public AlertMailer(AlertConfiguration cfg) {
+  public AlertMailer(AlertConfiguration cfg) throws IOException {
     log = LoggerFactory.getLogger(AlertMailer.class);
     this.cfg = cfg;
 
-    try {
-      smtpCreds = RuntimeSecrets.interpretSecret(cfg.getSmtpCredentials(), cfg.getGcpProject());
-    } catch (IOException exc) {
-      throw new RuntimeException(exc.getMessage());
-    }
-
+    smtpCreds = RuntimeSecrets.interpretSecret(cfg.getSmtpCredentials(), cfg.getGcpProject());
     templateManager = new TemplateManager(cfg);
   }
 
