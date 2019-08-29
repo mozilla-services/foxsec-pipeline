e7ed2cf47d3a8505a6c2a40822ffadfb9975e691
Aaron Meihm <ameihm@mozilla.com>
2019-08-29 12:49:23 -0500

    simplex multi-input parse test

--- src/test/java/com/mozilla/secops/TestInputTypeFileMulti.java
+++ src/test/java/com/mozilla/secops/TestInputTypeFileMulti.java
@@ -1,6 +1,9 @@
 package com.mozilla.secops;
 
 import com.mozilla.secops.input.Input;
+import com.mozilla.secops.input.InputElement;
+import com.mozilla.secops.parser.Event;
+import com.mozilla.secops.parser.ParserCfg;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.testing.PAssert;
 import org.apache.beam.sdk.testing.TestPipeline;
@@ -37,6 +40,25 @@ public class TestInputTypeFileMulti {
     PCollection<Long> count = results.apply(Count.globally());
 
     PAssert.thatSingleton(count).isEqualTo(30L);
+
+    pipeline.run().waitUntilFinish();
+  }
+
+  @Test
+  public void readTextTestParsingElement() throws Exception {
+    Input input =
+        new Input()
+            .simplex()
+            .withInputElement(
+                new InputElement(Input.SIMPLEX_DEFAULT_ELEMENT)
+                    .addFileInput("./target/test-classes/testdata/inputtype_buffer1.txt")
+                    .addFileInput("./target/test-classes/testdata/inputtype_buffer2.txt")
+                    .setParserConfiguration(new ParserCfg()));
+
+    PCollection<Event> results = pipeline.apply(input.simplexRead());
+    PCollection<Long> count = results.apply(Count.globally());
+
+    PAssert.thatSingleton(count).isEqualTo(10L);
 
     pipeline.run().waitUntilFinish();
   }
