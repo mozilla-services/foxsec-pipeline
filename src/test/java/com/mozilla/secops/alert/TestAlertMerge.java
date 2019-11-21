7e22d644055402c61c378c8a02a7afc8973e9f4f
Katie Kleemola <kkleemola@mozilla.com>
2019-11-21 13:54:56 -0500

    Revert "fix test package names"
    This reverts commit 31dcadeec5800700c900c50e0734571ccf304150.

--- src/test/java/com/mozilla/secops/alert/TestAlertMerge.java
+++ src/test/java/com/mozilla/secops/alert/TestAlertMerge.java
@@ -1,4 +1,4 @@
-package com.mozilla.secops.alert;
+package com.mozilla.secops.customs;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
--- src/test/java/com/mozilla/secops/awsbehavior/TestAwsBehavior.java
+++ src/test/java/com/mozilla/secops/awsbehavior/TestAwsBehavior.java
@@ -1,4 +1,4 @@
-package com.mozilla.secops.awsbehavior;
+package com.mozilla.secops.customs;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
