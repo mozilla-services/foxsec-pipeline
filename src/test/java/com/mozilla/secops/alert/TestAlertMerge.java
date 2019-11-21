31dcadeec5800700c900c50e0734571ccf304150
Katie Kleemola <kkleemola@mozilla.com>
2019-11-21 13:29:13 -0500

    fix test package names

--- src/test/java/com/mozilla/secops/alert/TestAlertMerge.java
+++ src/test/java/com/mozilla/secops/alert/TestAlertMerge.java
@@ -1,4 +1,4 @@
-package com.mozilla.secops.customs;
+package com.mozilla.secops.alert;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
--- src/test/java/com/mozilla/secops/awsbehavior/TestAwsBehavior.java
+++ src/test/java/com/mozilla/secops/awsbehavior/TestAwsBehavior.java
@@ -1,4 +1,4 @@
-package com.mozilla.secops.customs;
+package com.mozilla.secops.awsbehavior;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
