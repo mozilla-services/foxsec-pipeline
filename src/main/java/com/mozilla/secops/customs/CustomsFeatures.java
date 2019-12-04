4603b0952c53426e244b497ea81b6c4c0e33cd3f
Aaron Meihm <ameihm@mozilla.com>
2019-12-04 09:54:24 -0600

    add CustomsFeatures
    Add new CustomsFeatures class that is intended for windowed feature
    extraction from FxaAuth event streams. Provides a unified summary format
    for use in various downstream transforms.

---
+++ src/main/java/com/mozilla/secops/customs/CustomsFeatures.java
@@ -0,0 +1,192 @@
+package com.mozilla.secops.customs;
+
+import com.mozilla.secops.parser.Event;
+import com.mozilla.secops.parser.FxaAuth;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/** CustomsFeatures describes the output of windowed feature extraction */
+public class CustomsFeatures implements Serializable {
+  private static final long serialVersionUID = 1L;
+
+  private ArrayList<Event> events;
+
+  private HashMap<String, Integer> sourceAddressEventCount;
+
+  private int totalLoginFailureCount;
+  private int totalLoginSuccessCount;
+  private int totalAccountCreateSuccess;
+  private int totalPasswordForgotSendCodeSuccess;
+  private int totalPasswordForgotSendCodeFailure;
+
+  @Override
+  public boolean equals(Object o) {
+    if (!(o instanceof CustomsFeatures)) {
+      return false;
+    }
+    CustomsFeatures t = (CustomsFeatures) o;
+    return events.equals(t.getEvents());
+  }
+
+  @Override
+  public int hashCode() {
+    return events.hashCode();
+  }
+
+  /**
+   * Merge this feature set with another one
+   *
+   * @param cf {@link CustomsFeatures} to merge into this object
+   */
+  public void merge(CustomsFeatures cf) {
+    events.addAll(cf.getEvents());
+
+    totalLoginFailureCount += cf.getTotalLoginFailureCount();
+    totalLoginSuccessCount += cf.getTotalLoginSuccessCount();
+    totalAccountCreateSuccess += cf.getTotalAccountCreateSuccess();
+    totalPasswordForgotSendCodeSuccess += cf.getTotalPasswordForgotSendCodeSuccess();
+    totalPasswordForgotSendCodeFailure += cf.getTotalPasswordForgotSendCodeFailure();
+
+    for (Map.Entry<String, Integer> entry : cf.getSourceAddressEventCount().entrySet()) {
+      Integer cur =
+          sourceAddressEventCount.containsKey(entry.getKey())
+              ? sourceAddressEventCount.get(entry.getKey())
+              : 0;
+      sourceAddressEventCount.put(entry.getKey(), cur + entry.getValue());
+    }
+  }
+
+  /**
+   * Get count of total events per source address
+   *
+   * @return HashMap
+   */
+  public HashMap<String, Integer> getSourceAddressEventCount() {
+    return sourceAddressEventCount;
+  }
+
+  /**
+   * Get total password forgot send code failure count for event set
+   *
+   * @return int
+   */
+  public int getTotalPasswordForgotSendCodeFailure() {
+    return totalPasswordForgotSendCodeFailure;
+  }
+
+  /**
+   * Get total password forgot send code success count for event set
+   *
+   * @return int
+   */
+  public int getTotalPasswordForgotSendCodeSuccess() {
+    return totalPasswordForgotSendCodeSuccess;
+  }
+
+  /**
+   * Get total login failure count for event set
+   *
+   * @return int
+   */
+  public int getTotalLoginFailureCount() {
+    return totalLoginFailureCount;
+  }
+
+  /**
+   * Get total login success count for event set
+   *
+   * @return int
+   */
+  public int getTotalLoginSuccessCount() {
+    return totalLoginSuccessCount;
+  }
+
+  /**
+   * Get total account create success count for event set
+   *
+   * @return int
+   */
+  public int getTotalAccountCreateSuccess() {
+    return totalAccountCreateSuccess;
+  }
+
+  /**
+   * Add a single event to the event list
+   *
+   * @param e Event
+   */
+  public void addEvent(Event e) {
+    events.add(e);
+
+    FxaAuth.EventSummary s = CustomsUtil.authGetEventSummary(e);
+    if (s != null) {
+      switch (s) {
+        case LOGIN_FAILURE:
+          totalLoginFailureCount++;
+          break;
+        case LOGIN_SUCCESS:
+          totalLoginSuccessCount++;
+          break;
+        case ACCOUNT_CREATE_SUCCESS:
+          totalAccountCreateSuccess++;
+          break;
+      }
+    }
+
+    String sa = CustomsUtil.authGetSourceAddress(e);
+    if (sa != null) {
+      Integer cnt = sourceAddressEventCount.containsKey(sa) ? sourceAddressEventCount.get(sa) : 0;
+      sourceAddressEventCount.put(sa, cnt + 1);
+    }
+  }
+
+  /**
+   * Get event list
+   *
+   * @return ArrayList
+   */
+  public ArrayList<Event> getEvents() {
+    return events;
+  }
+
+  /**
+   * Set event list
+   *
+   * @param events ArrayList
+   */
+  public void setEvents(ArrayList<Event> events) {
+    this.events = events;
+  }
+
+  /**
+   * Get all events from event list of a certain type
+   *
+   * @param t {@link FxaAuth.EventSummary}
+   * @return ArrayList
+   */
+  public ArrayList<Event> getEventsOfType(FxaAuth.EventSummary t) {
+    ArrayList<Event> ret = new ArrayList<>();
+    for (Event i : events) {
+      FxaAuth.EventSummary s = CustomsUtil.authGetEventSummary(i);
+      if ((s == null) || (!s.equals(t))) {
+        continue;
+      }
+      ret.add(i);
+    }
+    return ret;
+  }
+
+  CustomsFeatures() {
+    events = new ArrayList<Event>();
+
+    sourceAddressEventCount = new HashMap<String, Integer>();
+
+    totalLoginFailureCount = 0;
+    totalLoginSuccessCount = 0;
+    totalAccountCreateSuccess = 0;
+    totalPasswordForgotSendCodeSuccess = 0;
+    totalPasswordForgotSendCodeFailure = 0;
+  }
+}
---
+++ src/test/java/com/mozilla/secops/customs/TestCustomsFeatures.java
@@ -0,0 +1,132 @@
+package com.mozilla.secops.customs;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import com.mozilla.secops.TestUtil;
+import com.mozilla.secops.parser.Event;
+import com.mozilla.secops.parser.ParserCfg;
+import com.mozilla.secops.parser.ParserDoFn;
+import com.mozilla.secops.parser.Payload;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import org.apache.beam.sdk.testing.PAssert;
+import org.apache.beam.sdk.testing.TestPipeline;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.values.KV;
+import org.apache.beam.sdk.values.PCollection;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class TestCustomsFeatures implements Serializable {
+  private static final long serialVersionUID = 1L;
+
+  @Rule public final transient TestPipeline p = TestPipeline.create();
+
+  @Test
+  public void testCustomsFeaturesCombine() throws Exception {
+    PCollection<String> input =
+        TestUtil.getTestInput("/testdata/customs_abuse_password_reset1.txt", p);
+
+    ParserCfg parserCfg = new ParserCfg();
+    parserCfg.setXffAddressSelector(new ArrayList<>(Arrays.asList(new String[] {"127.0.0.1/32"})));
+
+    PCollection<KV<String, CustomsFeatures>> res =
+        input
+            .apply(ParDo.of(new ParserDoFn().withConfiguration(parserCfg)))
+            .apply(
+                ParDo.of(
+                    new DoFn<Event, KV<String, Event>>() {
+                      private static final long serialVersionUID = 1L;
+
+                      @ProcessElement
+                      public void processElement(ProcessContext c) {
+                        if (!c.element().getPayloadType().equals(Payload.PayloadType.FXAAUTH)) {
+                          return;
+                        }
+                        c.output(KV.of(CustomsUtil.authGetSourceAddress(c.element()), c.element()));
+                      }
+                    }))
+            .apply(new CustomsFeaturesCombiner());
+
+    PAssert.that(res)
+        .satisfies(
+            x -> {
+              int tcnt = 0;
+
+              for (KV<String, CustomsFeatures> v : x) {
+                CustomsFeatures col = v.getValue();
+                if (v.getKey().equals("10.0.0.1")) {
+                  assertEquals(5, col.getEvents().size());
+                } else if (v.getKey().equals("10.0.0.2")) {
+                  assertEquals(4, col.getEvents().size());
+                } else {
+                  fail("unexpected key");
+                }
+                tcnt++;
+              }
+              assertEquals(2, tcnt);
+              return null;
+            });
+
+    p.run().waitUntilFinish();
+  }
+
+  @Test
+  public void testCustomsFeaturesEmailKeyLoginFailure() throws Exception {
+    PCollection<String> input =
+        TestUtil.getTestInput("/testdata/customs_rl_badlogin_simple1.txt", p);
+
+    ParserCfg parserCfg = new ParserCfg();
+    parserCfg.setXffAddressSelector(new ArrayList<>(Arrays.asList(new String[] {"127.0.0.1/32"})));
+
+    PCollection<KV<String, CustomsFeatures>> res =
+        input
+            .apply(ParDo.of(new ParserDoFn().withConfiguration(parserCfg)))
+            .apply(
+                ParDo.of(
+                    new DoFn<Event, KV<String, Event>>() {
+                      private static final long serialVersionUID = 1L;
+
+                      @ProcessElement
+                      public void processElement(ProcessContext c) {
+                        if (!c.element().getPayloadType().equals(Payload.PayloadType.FXAAUTH)) {
+                          return;
+                        }
+                        c.output(KV.of(CustomsUtil.authGetEmail(c.element()), c.element()));
+                      }
+                    }))
+            .apply(new CustomsFeaturesCombiner());
+
+    PAssert.that(res)
+        .satisfies(
+            x -> {
+              int tcnt = 0;
+
+              for (KV<String, CustomsFeatures> v : x) {
+                CustomsFeatures col = v.getValue();
+                if (v.getKey().equals("kirk@mozilla.com")) {
+                  assertEquals(12, col.getEvents().size());
+                  assertEquals(12, col.getTotalLoginFailureCount());
+                  assertEquals(0, col.getTotalLoginSuccessCount());
+                  assertEquals(10, col.getSourceAddressEventCount().size());
+                } else if (v.getKey().equals("spock@mozilla.com")) {
+                  assertEquals(12, col.getEvents().size());
+                  assertEquals(10, col.getTotalLoginFailureCount());
+                  assertEquals(0, col.getTotalLoginSuccessCount());
+                  assertEquals(12, (int) col.getSourceAddressEventCount().get("216.160.83.56"));
+                  assertEquals(1, col.getSourceAddressEventCount().size());
+                } else {
+                  fail("unexpected key");
+                }
+                tcnt++;
+              }
+              assertEquals(2, tcnt);
+              return null;
+            });
+
+    p.run().waitUntilFinish();
+  }
+}
