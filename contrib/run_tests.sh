e0dfb739a049fa336b860410d584491c499e9ea3
Aaron Meihm <ameihm@mozilla.com>
2020-02-05 13:58:53 -0600

    contrib test execution in foxsec-pipeline-base
    Modify repository layout so imported contrib code can be tested under
    foxsec-pipeline-base docker image.
    
    Upgrades version of go installed in base image.

--- contrib/run_tests.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker run -it --rm \
-  -v $PWD:/go/src/github.com/mozilla-services/foxsec-pipeline-contrib \
-  foxsec-pipeline-contrib:latest \
-  bash -c "go test ./...; cd duopull/; go test; cd ..; cd slackbot-background; go test"
