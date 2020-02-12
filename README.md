56e04a76d779aec9a9a5df527e1bad0798f8ee68
AJ Bahnken <aj@ajvb.me>
2020-02-12 09:30:09 -0800

    Watchlist CLI
    Add a CLI to Watchlist that allows of creation of watchlist entry json
    blobs with support for submitting them to datastore.
    
    Additionally, include documentation for using the Minfraud CLI.

--- README.md
+++ README.md
@@ -23,13 +23,11 @@ docker build -f Dockerfile-base -t foxsec-pipeline-base:latest .
 bin/m test -Dtest=ParserTest
 ```
 
-## Usage
+## CLI Usage
 
-### Pipeline Runtime Secrets
+### Pipeline [RuntimeSecrets](https://mozilla-services.github.io/foxsec-pipeline/secops-beam/com/mozilla/secops/crypto/RuntimeSecrets.html)
 
-#### Generating locally for use in PipelineOptions
-
-Pipeline runtime secrets can be generated locally using the main method in the `RuntimeSecrets` class.
+Pipeline runtime secrets can be generated locally using the main method in the [`RuntimeSecrets`](https://mozilla-services.github.io/foxsec-pipeline/secops-beam/com/mozilla/secops/crypto/RuntimeSecrets.html) class.
 
 ```bash
 bin/m compile exec:java -Dexec.mainClass=com.mozilla.secops.crypto.RuntimeSecrets -Dexec.args='-i testkey -k dataflow -p my-gcp-dataflow-project -r dataflow'
@@ -41,6 +39,61 @@ using these identifiers when the pipeline is executing.
 
 The output of the command can be prefixed with `cloudkms://` in an option to enable runtime decryption of the secrets
 during pipeline execution.
+
+### Interacting with Minfraud
+
+Reputation data can be fetched from Minfraud locally using the main method in the [`Minfraud`](https://mozilla-services.github.io/foxsec-pipeline/secops-beam/com/mozilla/secops/Minfraud.html) class.
+
+You must provide the accountid and licensekey plus the IP and/or email you want to get reputation data for. `--accountid` and `--licensekey` can either be provided directly or provided as RuntimeSecrets (`cloudkms://...`).
+
+```bash
+bin/m exec:java \
+  -Dexec.mainClass="com.mozilla.secops.Minfraud" \
+  -Dexec.args="-p my-gcp-dataflow-project --accountid 'cloudkms://...' --licensekey 'cloudkms://...' --ip '8.8.8.8' --email 'example@example.com'"
+```
+
+### Creating Watchlist entries
+
+Watchlist entries can be created locally using the main method in the [`Watchlist`](https://mozilla-services.github.io/foxsec-pipeline/secops-beam/com/mozilla/secops/Watchlist.html) class.
+
+You must also prefix your command with `WITHOUT_DAEMONS=true` so that the entry won't be submitted to the Datastore emulator running within the container.
+
+```
+usage: Watchlist
+ -c,--createdby <arg>
+ -ne,--neverexpires     Watchlist entry never expires (compared to default
+                        of 2 weeks)
+ -o,--object <arg>      Object to watch. Can be an IP or email.
+ -p,--project <arg>     GCP project name (required if submitting to
+                        Datastore)
+ -s,--severity <arg>    Severity of Watchlist entry. Can be 'info',
+                        'warn', or 'crit'
+ -su,--submit           Submit Watchlist entry to Datastore rather than
+                        emit json
+ -t,--type <arg>        Type of object to watch. Can be 'ip' or 'email'
+```
+
+#### Example of creating entry without submitting to Datastore
+```bash
+$ bin/m exec:java -Dexec.mainClass="com.mozilla.secops.Watchlist" -Dexec.args="--object '127.0.0.1' --type 'ip' --createdby 'example@example.com' --severity 'info'"
+
+{"type":"ip","severity":"info","expires_at":"2020-02-26T17:45:01.399Z","created_by":"example@example.com","object":"127.0.0.1"}
+```
+
+#### Example of submitting to Datastore
+```bash
+$ WITHOUT_DAEMONS=true bin/m exec:java -Dexec.mainClass="com.mozilla.secops.Watchlist" -Dexec.args="--object '127.0.0.1' --type 'ip' --createdby 'example@example.com' --severity 'info' --project foxsec-pipeline-nonprod --submit"
+
+Feb 12, 2020 5:41:44 PM com.mozilla.secops.state.State initialize
+INFO: Initializing new state interface using com.mozilla.secops.state.DatastoreStateInterface
+Feb 12, 2020 5:41:45 PM com.mozilla.secops.state.StateCursor set
+INFO: Writing state for 127.0.0.1
+Feb 12, 2020 5:41:45 PM com.mozilla.secops.state.State done
+INFO: Closing state interface com.mozilla.secops.state.DatastoreStateInterface
+Successfully submitted watchlist entry to foxsec-pipeline-nonprod
+{"type":"ip","severity":"info","expires_at":"2020-02-26T17:41:43.919Z","created_by":"example@example.com","object":"127.0.0.1"}
+```
+
 
 ## Documentation
 
--- src/main/java/com/mozilla/secops/Watchlist.java
+++ src/main/java/com/mozilla/secops/Watchlist.java
@@ -1,11 +1,22 @@
 package com.mozilla.secops;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.joda.JodaModule;
 import com.mozilla.secops.alert.Alert;
 import com.mozilla.secops.state.DatastoreStateInterface;
 import com.mozilla.secops.state.State;
+import com.mozilla.secops.state.StateCursor;
 import com.mozilla.secops.state.StateException;
 import java.util.Objects;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -145,6 +156,23 @@ public class Watchlist {
     public int hashCode() {
       return Objects.hash(getType() + getObject());
     }
+
+    /**
+     * Return JSON string representation.
+     *
+     * @return String or null if serialization fails.
+     */
+    public String toJSON() {
+      ObjectMapper mapper = new ObjectMapper();
+      mapper.registerModule(new JodaModule());
+      mapper.configure(
+          com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+      try {
+        return mapper.writeValueAsString(this);
+      } catch (JsonProcessingException exc) {
+        return null;
+      }
+    }
   }
 
   /**
@@ -258,5 +286,119 @@ public class Watchlist {
   public void done() {
     ipState.done();
     emailState.done();
+  }
+
+  /** main routine used to create watchlist entries. */
+  public static void main(String[] args) throws Exception {
+    Options options = new Options();
+
+    Option object = new Option("o", "object", true, "Object to watch. Can be an IP or email.");
+    object.setRequired(true);
+    options.addOption(object);
+
+    Option type = new Option("t", "type", true, "Type of object to watch. Can be 'ip' or 'email'");
+    type.setRequired(true);
+    options.addOption(type);
+
+    Option createdby = new Option("c", "createdby", true, "");
+    createdby.setRequired(true);
+    options.addOption(createdby);
+
+    Option severity =
+        new Option(
+            "s", "severity", true, "Severity of Watchlist entry. Can be 'info', 'warn', or 'crit'");
+    severity.setRequired(true);
+    options.addOption(severity);
+
+    Option neverexpires =
+        new Option(
+            "ne",
+            "neverexpires",
+            false,
+            "Watchlist entry never expires (compared to default of 14 days)");
+    options.addOption(neverexpires);
+
+    Option project =
+        new Option("p", "project", true, "GCP project name (required if submitting to Datastore)");
+    options.addOption(project);
+
+    Option submit =
+        new Option(
+            "su", "submit", false, "Submit Watchlist entry to Datastore rather than emit json");
+    options.addOption(submit);
+
+    CommandLineParser parser = new DefaultParser();
+    HelpFormatter fmt = new HelpFormatter();
+    CommandLine cmd = null;
+    try {
+      cmd = parser.parse(options, args);
+    } catch (ParseException e) {
+      System.out.println(e.getMessage());
+      fmt.printHelp("Watchlist", options);
+      System.exit(1);
+    }
+
+    Watchlist.WatchlistEntry we = new Watchlist.WatchlistEntry();
+    if (!cmd.getOptionValue("type").equals("email") && !cmd.getOptionValue("type").equals("ip")) {
+      System.out.println("Unsupported --type: " + cmd.getOptionValue("type"));
+      fmt.printHelp("Watchlist", options);
+      System.exit(1);
+    }
+
+    we.setType(cmd.getOptionValue("type"));
+    we.setObject(cmd.getOptionValue("object"));
+    we.setCreatedBy(cmd.getOptionValue("createdby"));
+
+    // Parse severity based on text arg
+    if (cmd.getOptionValue("severity").equals("info")) {
+      we.setSeverity(Alert.AlertSeverity.INFORMATIONAL);
+    } else if (cmd.getOptionValue("severity").equals("info")) {
+      we.setSeverity(Alert.AlertSeverity.WARNING);
+    } else if (cmd.getOptionValue("severity").equals("info")) {
+      we.setSeverity(Alert.AlertSeverity.CRITICAL);
+    } else {
+      System.out.println("Got unknown severity option: " + cmd.getOptionValue("severity"));
+      fmt.printHelp("Watchlist", options);
+      System.exit(1);
+    }
+
+    // Set expiration
+    if (cmd.hasOption("neverexpires")) {
+      DateTime d = new DateTime();
+      d = d.plusYears(50);
+      we.setExpiresAt(d);
+    } else {
+      DateTime d = new DateTime();
+      d = d.plusDays(14);
+      we.setExpiresAt(d);
+    }
+
+    // Submit to datastore if option(s) are present
+    if (cmd.hasOption("su")) {
+      if (cmd.getOptionValue("p", null) == null) {
+        System.out.println("--project required with --submit");
+        fmt.printHelp("Watchlist", options);
+        System.exit(1);
+      }
+
+      String kind = watchlistIpKind;
+      if (we.getType().equals("email")) {
+        kind = watchlistEmailKind;
+      }
+
+      State s =
+          new State(
+              new DatastoreStateInterface(
+                  kind, Watchlist.watchlistDatastoreNamespace, cmd.getOptionValue("p")));
+      s.initialize();
+      StateCursor c = s.newCursor();
+      c.set(we.getObject(), we);
+      c.commit();
+      s.done();
+      System.out.println("Successfully submitted watchlist entry to " + cmd.getOptionValue("p"));
+    }
+
+    String weJson = we.toJSON();
+    System.out.println(weJson);
   }
 }
