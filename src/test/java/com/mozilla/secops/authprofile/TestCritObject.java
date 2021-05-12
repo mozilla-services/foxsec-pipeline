7f95c36610f28827507218d97cedd753cc787413
Katie Kleemola <kkleemola@mozilla.com>
2021-05-11 20:39:01 -0400

    fix tests

--- src/test/java/com/mozilla/secops/authprofile/TestCritObject.java
+++ src/test/java/com/mozilla/secops/authprofile/TestCritObject.java
@@ -343,7 +343,7 @@ public class TestCritObject {
   public void critObjectWithSessionBasedSuppressionTest() throws Exception {
     AuthProfile.AuthProfileOptions options = getTestOptions();
 
-    String[] eb1 = TestUtil.getTestInputArray("/testdata/authprof_critobj8.txt");
+    String[] eb1 = TestUtil.getTestInputArray("/testdata/authprof_critobj5.txt");
     TestStream<String> s =
         TestStream.create(StringUtf8Coder.of())
             .advanceWatermarkTo(new Instant(0L))
@@ -421,6 +421,113 @@ public class TestCritObject {
               // We should have 2; 3 alerts would have been generated in total with the second one
               // being suppressed
               assertEquals(2L, cnt);
+              return null;
+            });
+
+    p.run().waitUntilFinish();
+  }
+
+  @Test
+  public void critObjectWithSessionBasedSuppressionWithChangingIpTest() throws Exception {
+    AuthProfile.AuthProfileOptions options = getTestOptions();
+
+    String[] eb1 = TestUtil.getTestInputArray("/testdata/authprof_critobj6.txt");
+    TestStream<String> s =
+        TestStream.create(StringUtf8Coder.of())
+            .advanceWatermarkTo(new Instant(0L))
+            .addElements(eb1[0], Arrays.copyOfRange(eb1, 1, eb1.length))
+            .advanceWatermarkToInfinity();
+
+    Input input = TestAuthProfileUtil.wiredInputStream(options, s);
+    PCollection<Alert> res = AuthProfile.processInput(p.apply(input.simplexReadRaw()), options);
+
+    PAssert.that(res)
+        .satisfies(
+            results -> {
+              long cntIp1 = 0;
+              long cntIp2 = 0;
+              long cfgTickCnt = 0;
+              for (Alert a : results) {
+                if (a.getMetadataValue(AlertMeta.Key.ALERT_SUBCATEGORY_FIELD)
+                        .equals("critical_object_analyze")
+                    && a.getMetadataValue(AlertMeta.Key.SOURCEADDRESS).equals("216.160.83.56")) {
+                  assertEquals(Alert.AlertSeverity.CRITICAL, a.getSeverity());
+                  assertEquals(
+                      "critical authentication event observed laforge@mozilla.com to "
+                          + "projects/test, 216.160.83.56 [Milton/US]",
+                      a.getSummary());
+                  assertThat(
+                      a.getPayload(),
+                      containsString(
+                          "This destination object is configured as a critical resource"));
+                  assertEquals(
+                      "critical_object_analyze",
+                      a.getMetadataValue(AlertMeta.Key.ALERT_SUBCATEGORY_FIELD));
+                  assertEquals(
+                      "section31@mozilla.com",
+                      a.getMetadataValue(AlertMeta.Key.NOTIFY_EMAIL_DIRECT));
+                  assertEquals("laforge@mozilla.com", a.getMetadataValue(AlertMeta.Key.USERNAME));
+                  assertEquals("projects/test", a.getMetadataValue(AlertMeta.Key.OBJECT));
+                  assertEquals("216.160.83.56", a.getMetadataValue(AlertMeta.Key.SOURCEADDRESS));
+                  assertEquals("Milton", a.getMetadataValue(AlertMeta.Key.SOURCEADDRESS_CITY));
+                  assertEquals("US", a.getMetadataValue(AlertMeta.Key.SOURCEADDRESS_COUNTRY));
+                  assertEquals("email/authprofile.ftlh", a.getEmailTemplate());
+                  assertEquals("slack/authprofile.ftlh", a.getSlackTemplate());
+                  assertEquals("auth_session", a.getMetadataValue(AlertMeta.Key.AUTH_ALERT_TYPE));
+                  cntIp1++;
+
+                } else if (a.getMetadataValue(AlertMeta.Key.ALERT_SUBCATEGORY_FIELD)
+                        .equals("critical_object_analyze")
+                    && a.getMetadataValue(AlertMeta.Key.SOURCEADDRESS).equals("216.160.83.50")) {
+                  assertEquals(Alert.AlertSeverity.CRITICAL, a.getSeverity());
+                  assertEquals(
+                      "critical authentication event observed laforge@mozilla.com to "
+                          + "projects/test, 216.160.83.50 [unknown/unknown]",
+                      a.getSummary());
+                  assertThat(
+                      a.getPayload(),
+                      containsString(
+                          "This destination object is configured as a critical resource"));
+                  assertEquals(
+                      "critical_object_analyze",
+                      a.getMetadataValue(AlertMeta.Key.ALERT_SUBCATEGORY_FIELD));
+                  assertEquals(
+                      "section31@mozilla.com",
+                      a.getMetadataValue(AlertMeta.Key.NOTIFY_EMAIL_DIRECT));
+                  assertEquals("laforge@mozilla.com", a.getMetadataValue(AlertMeta.Key.USERNAME));
+                  assertEquals("projects/test", a.getMetadataValue(AlertMeta.Key.OBJECT));
+                  assertEquals("216.160.83.50", a.getMetadataValue(AlertMeta.Key.SOURCEADDRESS));
+                  assertEquals("unknown", a.getMetadataValue(AlertMeta.Key.SOURCEADDRESS_CITY));
+                  assertEquals("unknown", a.getMetadataValue(AlertMeta.Key.SOURCEADDRESS_COUNTRY));
+                  assertEquals("email/authprofile.ftlh", a.getEmailTemplate());
+                  assertEquals("slack/authprofile.ftlh", a.getSlackTemplate());
+                  assertEquals("auth_session", a.getMetadataValue(AlertMeta.Key.AUTH_ALERT_TYPE));
+                  cntIp2++;
+
+                } else if (a.getMetadataValue(AlertMeta.Key.ALERT_SUBCATEGORY_FIELD)
+                    .equals("cfgtick")) {
+                  cfgTickCnt++;
+                  assertEquals("authprofile-cfgtick", a.getCategory());
+                  assertEquals(
+                      "^projects/test$, super-important-account",
+                      a.getCustomMetadataValue("critObjects"));
+                  assertEquals(
+                      "section31@mozilla.com",
+                      a.getCustomMetadataValue("criticalNotificationEmail"));
+                  assertEquals("^riker@mozilla.com$", a.getCustomMetadataValue("ignoreUserRegex"));
+                  assertEquals("5", a.getCustomMetadataValue("generateConfigurationTicksMaximum"));
+                  assertEquals(
+                      "Alert via section31@mozilla.com immediately on auth events to specified objects: [^projects/test$, super-important-account]",
+                      a.getCustomMetadataValue("heuristic_CritObjectAnalyze"));
+                } else {
+                  fail("unexpected category");
+                }
+              }
+              assertEquals(5L, cfgTickCnt);
+              // We should have 2; 3 alerts would have been generated in total with the second one
+              // being suppressed
+              assertEquals(1L, cntIp1);
+              assertEquals(1L, cntIp2);
               return null;
             });
 
